eec8ab2fde3a680c624e644a68a0a77d3c986bd8
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2020-08-14 13:27:06 +0200

    first commit

---
+++ Book.java
@@ -0,0 +1,35 @@
+package be.intecbrussel;
+
+import java.time.LocalDate;
+
+public class Book {
+
+    String title;
+    String genre;
+    private LocalDate releaseDate;
+    Person author;
+
+    public Book(String title, String genre, LocalDate releaseDate, Person author) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+        this.author = author;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public Person getAuthor() {
+        return author;
+    }
+}
---
+++ Person.java
@@ -0,0 +1,46 @@
+package be.intecbrussel;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Person {
+
+    String firstName;
+    String lastName;
+    private LocalDate dateOfBirth;
+
+    public Person(String firstName, String lastName, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return Objects.equals(firstName, person.firstName) &&
+                Objects.equals(lastName, person.lastName) &&
+                Objects.equals(dateOfBirth, person.dateOfBirth);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, dateOfBirth);
+    }
+}
