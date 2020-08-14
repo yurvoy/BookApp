125899400e1daa68a53da9fe10514dd9fae31fe4
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2020-08-14 14:29:24 +0200

    nieuw

--- Utilities.java
+++ Utilities.java
@@ -1,14 +1,31 @@
 package be.intecbrussel;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Utilities {
 
+//    public static void printYoungestWriter(Book[] books ){
+//        int youngestIndex = 0;
+//        for (int i = 1; i < books.length; i++) {
+//            if(books[i].getAuthor().getDateOfBirth().isAfter(books[i-1].getAuthor().getDateOfBirth())){
+//                youngestIndex = i;
+//            }
+//        }
+//        System.out.println(books[youngestIndex]);
+//    }
+
     public static void printYoungestWriter(Book[] books ){
-        int youngestIndex = 0;
-        for (int i = 1; i < books.length; i++) {
-            if(books[i].getAuthor().getDateOfBirth().isAfter(books[i-1].getAuthor().getDateOfBirth())){
-                youngestIndex = i;
-            }
+        List<Person> authors = new ArrayList<>();
+        for (Book book : books) {
+            authors.add(book.author);
         }
-        System.out.println(books[youngestIndex]);
+        Person youngest =  authors.stream()
+                .max(Comparator.comparing(Person::getDateOfBirth))
+                .get();
+        System.out.println("Youngest Author : " + youngest);
     }
+
+
 }
